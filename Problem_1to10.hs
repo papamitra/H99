d5ee2aa76443d223a4ec8b4408e1fbf2c42f0815
papamitra <papamitra@gmail.com>
2013-06-02 18:47:18 +0900

    add Problem No.

--- Problem_1to10.hs
+++ Problem_1to10.hs
@@ -1,7 +1,10 @@
+
+-- Problem 1
 myLast :: [a] -> a
 myLast (x:[]) = x 
 myLast (x:xs) = myLast xs
 
+-- Problem 2
 -- mindmind wrote
 myButLast :: [a] -> a
 myButLast [] =  error "out of bounds"
@@ -9,11 +12,13 @@ myButLast (x:[]) = error "out of bounds"
 myButLast (x:_:[]) = x
 myButLast (x:xs) = myButLast xs
 
+-- Problem 3
 elementAt :: [a] -> Int -> a
 elementAt [] _ = error "out of bounds"
 elementAt (x:_) 1 = x
 elementAt (_:xs) n = elementAt xs (n-1)
 
+-- Problem 4
 myLength :: [a] -> Integer
 myLength []     = 0
 myLength (x:xs) = 1 + myLength xs
