089fd85bf45d3f135fa17f17c4bedd8ca7a05ca7
mind mind <minddnim@gmail.com>
2013-06-16 12:39:26 +0900

    5 Problem 5
    (*) Reverse a list.
    
    8 Problem 8
    (**) Eliminate consecutive duplicates of list elements.

--- Problem_1to10.hs
+++ Problem_1to10.hs
@@ -5,7 +5,6 @@ myLast (x:[]) = x
 myLast (x:xs) = myLast xs
 
 -- Problem 2
--- mindmind wrote
 myButLast :: [a] -> a
 myButLast [] =  error "out of bounds"
 myButLast (x:[]) = error "out of bounds"
@@ -23,13 +22,27 @@ myLength :: [a] -> Integer
 myLength []     = 0
 myLength (x:xs) = 1 + myLength xs
 
+-- Problem 5
+myReverse :: [a] -> [a]
+myReverse (x:xs) = chg xs [x]
+  where chg [] ret = ret
+        chg (x:xs) ret = chg xs (x : ret)
+
 -- Problem 6
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome xs = xs == (reverse xs)
 
 -- Problem 7
 data NestedList a = Elem a | List [NestedList a]
-                    deriving (Show)
+--                    deriving (Show)
 flatten :: NestedList a -> [a]
 flatten (Elem e) = [e]
 flatten (List l) = concat (map flatten l)
+
+-- Problem 8
+compress :: (Eq a) => [a] -> [a]
+compress [] = []
+compress (x:xs) = foldr (compress') [] xs
+  where compress' x ys | (ys == []) = [x]
+                       | (x == head ys)  = ys
+                       | otherwise = x : ys 
