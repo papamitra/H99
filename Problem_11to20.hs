1b3555660392c5d0202ca591e7b8501062b0d149
mind mind <minddnim@gmail.com>
2013-07-13 13:35:59 +0900

    16解いた！

--- Problem_11to20.hs
+++ Problem_11to20.hs
@@ -1,3 +1,10 @@
 -- Problem 14
 dupli :: [a] -> [a]
 dupli xs = reverse (foldl (\x y -> y:y:x) [] xs)
+
+-- Problem 16
+dropEvery :: String -> Int -> String
+dropEvery xs n = reverse $ dropEvery' xs n 1 ""
+  where dropEvery' [] n i ret = ret
+        dropEvery' (x:xs) n i ret | ((i `mod` n) == 0) = dropEvery' xs n 1 ret
+                                  | otherwise = dropEvery' xs n (i+1) (x:ret) 
