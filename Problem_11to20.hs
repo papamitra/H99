3e7429ff8dd202ce10ad40babf0aec4c46dcc1b0
mind mind <minddnim@gmail.com>
2013-07-28 05:21:43 +0900

    15解いた

--- Problem_11to20.hs
+++ Problem_11to20.hs
@@ -2,6 +2,12 @@
 dupli :: [a] -> [a]
 dupli xs = reverse (foldl (\x y -> y:y:x) [] xs)
 
+-- Problem 15
+repli :: String -> Int -> String
+repli str n = foldr (myRepeat n) [] str
+  where myRepeat 0 c ret = ret
+        myRepeat n c ret = myRepeat (n-1) c (c:ret)
+
 -- Problem 16
 dropEvery :: String -> Int -> String
 dropEvery xs n = reverse $ dropEvery' xs n 1 ""
