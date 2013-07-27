41ee75ffe2f9527d0d4e28b26f9a877219498e87
mind mind <minddnim@gmail.com>
2013-07-28 06:06:58 +0900

    問題15から18までの
    引数の型をStringから[a]に変更

--- Problem_11to20.hs
+++ Problem_11to20.hs
@@ -3,15 +3,25 @@ dupli :: [a] -> [a]
 dupli xs = reverse (foldl (\x y -> y:y:x) [] xs)
 
 -- Problem 15
-repli :: String -> Int -> String
-repli str n = foldr (myRepeat n) [] str
-  where myRepeat 0 c ret = ret
-        myRepeat n c ret = myRepeat (n-1) c (c:ret)
+repli :: [a] -> Int -> [a]
+repli xs n = foldr (myRepeat n) [] xs
+  where myRepeat 0 x ret = ret
+        myRepeat n x ret = myRepeat (n-1) x (x:ret)
 
 -- Problem 16
-dropEvery :: String -> Int -> String
-dropEvery xs n = reverse $ dropEvery' xs n 1 ""
+dropEvery :: [a] -> Int -> [a]
+dropEvery xs n = reverse $ dropEvery' xs n 1 []
   where dropEvery' [] n i ret = ret
         dropEvery' (x:xs) n i ret | ((i `mod` n) == 0) = dropEvery' xs n 1 ret
                                   | otherwise = dropEvery' xs n (i+1) (x:ret) 
+
+-- Problem 17
+split :: [a] -> Int -> ([a], [a])
+split xs n = ((take n xs), (drop n xs))
+
+-- Problem 18
+slice :: [a] -> Int -> Int -> [a]
+slice xs l r = headRemove (l-1) $ take r xs
+  where headRemove n str@(x:xs) | n <= 0 = str
+                                | otherwise =  headRemove (n-1) xs
 
